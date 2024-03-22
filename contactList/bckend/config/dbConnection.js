346b6abc7c441bb92d3f8cee84779e25dc3e3537
btaanish <87608292+btaanish@users.noreply.github.com>
2024-03-22 16:26:02 +0800

    Update dbConnection.js

--- contactList/bckend/config/dbConnection.js
+++ contactList/bckend/config/dbConnection.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log(
-      "Database connected: ",
-      connect.connection.host,
-      connect.connection.name
-    );
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`Database connected: ${connect.connection.host}`);
   } catch (err) {
-    console.log(err);
+    console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDb;
+export default connectDb;
