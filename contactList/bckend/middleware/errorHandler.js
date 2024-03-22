535232b3bc439380c30d7b8813bb43538d929462
btaanish <87608292+btaanish@users.noreply.github.com>
2024-03-22 16:23:54 +0800

    Update errorHandler.js

--- contactList/bckend/middleware/errorHandler.js
+++ contactList/bckend/middleware/errorHandler.js
@@ -1,42 +1,38 @@
-const { constants } = require("../constants");
+import { constants } from '../constants.js';
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+
+  let response = {
+    title: "",
+    message: err.message,
+    stackTrace: process.env.NODE_ENV === 'production' ? null : err.stack,
+  };
+
   switch (statusCode) {
     case constants.VALIDATION_ERROR:
-      res.json({
-        title: "Validation Failed",
-        message: err.message,
-        stackTrace: err.stack,
-      });
+      response.title = "Validation Failed";
       break;
     case constants.NOT_FOUND:
-      res.json({
-        title: "Not Found",
-        message: err.message,
-        stackTrace: err.stack,
-      });
+      response.title = "Not Found";
+      break;
     case constants.UNAUTHORIZED:
-      res.json({
-        title: "Unauthorized",
-        message: err.message,
-        stackTrace: err.stack,
-      });
+      response.title = "Unauthorized";
+      break;
     case constants.FORBIDDEN:
-      res.json({
-        title: "Forbidden",
-        message: err.message,
-        stackTrace: err.stack,
-      });
+      response.title = "Forbidden";
+      break;
     case constants.SERVER_ERROR:
-      res.json({
-        title: "Server Error",
-        message: err.message,
-        stackTrace: err.stack,
-      });
+      response.title = "Server Error";
+      break;
     default:
-      console.log("No Error, All good !");
+      console.log("No specific error handling case matched. Defaulting.");
+      response.title = "Error";
       break;
   }
+
+  res.json(response);
 };
 
-module.exports = errorHandler;
+export default errorHandler;
