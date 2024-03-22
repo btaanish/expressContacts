b664816994e61a4b4817227f6c368d6ad82ec73a
btaanish <87608292+btaanish@users.noreply.github.com>
2024-03-22 16:24:12 +0800

    Update validateTokenHandler.js

--- contactList/bckend/middleware/validateTokenHandler.js
+++ contactList/bckend/middleware/validateTokenHandler.js
@@ -1,25 +1,30 @@
-const asyncHandler = require("express-async-handler");
-const jwt = require("jsonwebtoken");
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
-  let authHeader = req.headers.Authorization || req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
-      if (err) {
-        res.status(401);
-        throw new Error("User is not authorized");
-      }
-      req.user = decoded.user;
-      next();
-    });
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!token) {
-      res.status(401);
-      throw new Error("User is not authorized or token is missing");
-    }
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
   }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("User is not authorized or token is missing");
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401);
+      throw new Error("User is not authorized, token is invalid or has expired");
+    }
+    req.user = decoded.user;
+    next();
+  });
 });
 
-module.exports = validateToken;
+export default validateToken;
